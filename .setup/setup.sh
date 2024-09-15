5279effae4e7efee19b1197c10b4e161b43e1a4b
Aditya Singh <1adityasingh@proton.me>
2024-09-15 13:46:02 +0530

    Install python3-neovim

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -73,6 +73,7 @@ cargo install ripdrag csvlens
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 rm -rf /opt/nvim
 tar -C /opt -xzf nvim-linux64.tar.gz
+dnf install -y python3-neovim
 
 # GNOME Settings
 dconf load / < $HOME/.config/gnome-settings/{application-settings,extensions,keybindings,shell-settings}.ini
