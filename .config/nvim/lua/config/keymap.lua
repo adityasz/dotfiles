d883caadce99f5f6a41d7d5fd6707319785f3ea4
Aditya Singh <1adityasingh@proton.me>
2025-04-27 12:45:35 +0530

    [nvim] Update keymap

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -5,22 +5,28 @@ map({'n', 'v', 'i', 'o'}, '<F1>', '<Esc>', opts)
 map('n', '<F2>', ':set relativenumber!<CR>', opts)
 map('n', '<F3>', ':noh<CR>', opts)
 
-map({'n', 'v'}, 'j', 'gj', opts)
-map({'n', 'v'}, 'k', 'gk', opts)
-map({'n', 'v'}, '$', 'g$', opts)
-map({'n', 'v'}, '0', 'g0', opts)
+-- map({'n', 'v'}, 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, silent = true, expr = true })
+-- map({'n', 'v'}, 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, silent = true, expr = true })
+map({'n', 'v'}, 'j', 'v:count == 0 ? "gj" : "m\'" . v:count . "j"',
+    { noremap = true, silent = true, expr = true })
+map({'n', 'v'}, 'k', 'v:count == 0 ? "gk" : "m\'" . v:count . "k"',
+    { noremap = true, silent = true, expr = true })
 map({'n', 'v'}, 'gj', 'j', opts)
 map({'n', 'v'}, 'gk', 'k', opts)
+
+map({'n', 'v'}, '$', 'g$', opts)
+map({'n', 'v'}, '0', 'g0', opts)
 map({'n', 'v'}, 'g$', '$', opts)
 map({'n', 'v'}, 'g0', '0', opts)
 
 map('v', '<C-c>', '"+y', opts)
-map('n', '<M-e>', ':Neotree toggle<CR> ', opts)
 map('n', '<leader>r', ':IncRename ', opts)
 map('n', '<leader>e', ':lua vim.diagnostic.open_float()<CR>', opts)
 map('n', '<leader>z', ':ZenMode<CR>', opts)
 map('n', '[e', ':lua vim.diagnostic.goto_prev()<CR>', opts)
 map('n', ']e', ':lua vim.diagnostic.goto_next()<CR>', opts)
+-- I no longer use Neotree as it conflicts with Oil.nvim.
+-- map('n', '<M-e>', ':Neotree toggle<CR> ', opts)
 
 -- Telescope buffers
 map('n', '<leader>t', function()
@@ -50,7 +56,7 @@ map('n', '<leader> ', function()
         layout_config = {
             height = math.max(files, 1) + 5,
             width = function()
-                return math.min(math.floor(0.8 * vim.o.columns), 40)
+                return math.min(math.floor(0.8 * vim.o.columns), 60)
             end,
         },
         hidden = false,  -- Explicitly set to not show hidden files
@@ -59,11 +65,15 @@ map('n', '<leader> ', function()
     })
     end, opts)
 
-map('n', '<leader>s', ":Telescope lsp_document_symbols<CR>", opts)
 map('n', '<leader>f', ":Telescope live_grep<CR>", opts)
 map('n', '<leader>w', ":Telescope lsp_workspace_symbols<CR>", opts)
 
-map('n', '<M-a>', ":CodeCompanionChat<CR>", opts)
+-- While both of the following are terrible, Telescope's built-in command at
+-- least doesn't miss stuff.
+map('n', '<leader>s', ":Telescope lsp_document_symbols<CR>", opts)
+-- map('n', '<leader>s', ":Telescope aerial<CR>", opts)
+
+map({ "n", "x" }, "<leader>h", function() require("ssr").open() end) -- ssr is useful in Typst, but it can cause crashes
 
 -- Handled by the kitty plugin
 -- map('n', '<C-j>', ':wincmd j<CR>', opts)
@@ -71,7 +81,6 @@ map('n', '<M-a>', ":CodeCompanionChat<CR>", opts)
 -- map('n', '<C-h>', ':wincmd h<CR>', opts)
 -- map('n', '<C-l>', ':wincmd l<CR>', opts)
 
--- S-F5 is F17
--- C-F5 is F29
+-- S-F5 is F17, C-F5 is F29
 -- map({'n', 'v', 'i'}, '<F17>', function() require('config.utils').run_single_file() end, {noremap = true, silent = true})
 -- map({'n', 'v', 'i'}, '<F29>', function() require('config.utils').debug_single_file() end, {noremap = true, silent = true})
