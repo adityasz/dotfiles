64dee6d16cb315983be9bf4704d34139ae0dfd1b
Aditya Singh <1adityasingh@proton.me>
2026-04-20 02:07:04 +0530

    [nvim/autopairs] Improve setup opts

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,11 +1,7 @@
 return {
     {
-        'windwp/nvim-autopairs',
+        "windwp/nvim-autopairs",
         event = "InsertEnter",
-        opts = {
-            ignored_next_char = "[%w%.]",
-            enable_check_bracket_line = false
-        },
         config = function()
             require('nvim-autopairs').setup({
                 disable_filetype = {"", "tex", "racket"},
