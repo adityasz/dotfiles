e2ef3bf08638f732ff73c13887b234390c097ace
Aditya Singh <1adityasingh@proton.me>
2026-04-20 01:30:49 +0530

    [nvim] Load UltiSnips for more file formats
    typst, snippets

--- .config/nvim/lua/plugins/ultisnips.lua
+++ .config/nvim/lua/plugins/ultisnips.lua
@@ -1,6 +1,6 @@
 return {
     'SirVer/ultisnips',
-    ft = { "tex", "plaintex", "latex" },
+    ft = { "tex", "plaintex", "latex", "typst", "snippets" },
     init = function()
         vim.g.UltiSnipsExpandTrigger = '<Tab>'
         vim.g.UltiSnipsJumpForwardTrigger = '<Tab>'
